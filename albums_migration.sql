ea3d56373cb3e9151ce7fdd6a10d5594832f451b
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-10-10 14:17:33 -0500

    complete 1st basic exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,15 +1,16 @@
-USE codeup_test_db2;
+USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-artist  VARCHAR(100),
-name    VARCHAR(100),
-release_date SMALLINT  UNSIGNED,
-sales   DECIMAL(14,2),
+artist  VARCHAR(100) NOT NULL,
+name    VARCHAR(100) NOT NULL,
+release_date INT(4)  UNSIGNED,
+sales   FLOAT(5,2) UNSIGNED,
 genre  VARCHAR(100),
 PRIMARY KEY(id)
+);
 
-)
+
 
---
+++ albums_seeder.sql
@@ -0,0 +1,194 @@
+use codeup_test_db;
+
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Eagles','	Their Greatest Hits (1971–1975)',	1976	,'Country rock,soft rock,folk rock	',41.20);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Michael Jackson','	Thriller',	1982	,'Pop,rock,R&B	',47.30);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales) values('AC/DC',	'Back in Black',	1980	,'Hard rock	',26.10);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Pink Floyd','The Dark Side of the Moon',	1973	,'Progressive rock	',24.20);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Whitney Houston/ Various artists','	The Bodyguard',	1992	,'R&B,soul,pop,soundtrack	',28.40);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Meat Loaf','	Bat Out of Hell',	1977	,'Hard rock,glam rock,heavy metal	',21.50);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Eagles','	Hotel California',	1976	,'Soft rock	',31.50);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Bee Gees/ Various artists','	Saturday Night Fever',	1977	,'Disco	',21.60);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Shania Twain','	Come On Over',	1997	,'Country,pop	',29.60);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Fleetwood Mac','	Rumours',	1977	,'Soft rock	',27.90);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Various artists','	Grease: The Original Soundtrack from the Motion Picture',	1978	,'Rock and roll	',14.40);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Led Zeppelin','	Led Zeppelin IV',	1971	,'Hard rock,heavy metal,folk rock	',29.00);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Michael Jackson','	Bad',	1987	,'Pop,funk,rock	',19.30);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Alanis Morissette','	Jagged Little Pill',	1995	,'Alternative rock	',24.40);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Celine Dion','	Falling into You',	1996	,'Pop,soft rock	',20.20);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('The Beatles','Sgt. Pepper''s Lonely Hearts Club Band',	1967	,'Rock	',18.20);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Various artists','	Dirty Dancing',	1987	,'Pop,rock,R&B	',17.90);
+
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Michael Jackson','	Dangerous',	1991	,'Rock,funk,pop	',17.30);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Adele','21',	2011	,'Pop,soul	',25.30);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Celine Dion','Let''s Talk About Love', 1997	,'Pop,soft rock	',19.30);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('The Beatles','1',	2000	,'Rock	',23.40);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('ABBA','	Gold: Greatest Hits',	1992	,'Pop,disco	',23.00);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Madonna','	The Immaculate Collection',	1990	,'Pop,dance	',19.40);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('The Beatles','	Abbey Road',	1969	,'Rock	',14.40);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Bruce Springsteen','Born in the U.S.A.',	1984	,'Heartland rock	',19.60);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Dire Straits','	Brothers in Arms',	1985	,'Roots rock,blues rock,soft rock	',17.70);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('James Horner','	Titanic: Music from the Motion Picture',	1997	,'Film score	',18.10);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Metallica','Metallica',	1991	,'Heavy metal	',25.20);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Nirvana', 'Nevermind',	1991	,'Grunge,alternative rock	',16.70);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Pink Floyd','The Wall',	1979	,'Progressive rock	',18.70);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Santana','	Supernatural',	1999	,'Latin rock	',20.50);
+insert into albums (artist,
+name,
+release_date,
+genre,
+sales)
+values('Guns N'' Roses','Appetite for Destruction',	1987	,'Hard rock	',21.9);
